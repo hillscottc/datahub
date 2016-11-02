f1d3d54e05f9844a03b1b32212c5d6706d15a342
Scott Hill <scott289@gmail.com>
2016-11-02 10:25:07 -0700

    using babel

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
-var express = require('express');
-var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import { buildSchema } from 'graphql';
 
 // Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
+const schema = buildSchema(`
   type Query {
     hello: String
   }
 `);
 
 // The root provides a resolver function for each API endpoint
-var root = {
+const root = {
   hello: () => {
     return 'Hello world!';
   },
 };
 
-var app = express();
+const app = express();
+
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
