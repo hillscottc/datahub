f72e13d6f23f8b89624f132de784a25fbbb90aad
Scott Hill <scott289@gmail.com>
2016-11-18 08:53:14 -0800

    no graphql

---
+++ database/bookshelf-db.js
@@ -0,0 +1,19 @@
+// import knex from 'knex';
+// import bookshelf from 'bookshelf';
+//
+// // Can't use config values here.
+// // import config  from '../config';
+//
+// const Bookshelf = bookshelf(knex(
+//   // {client: 'pg', connection: process.env.DATABASE_URL || 'postgres:///hills120' }
+//   {client: 'pg', connection: process.env.DATABASE_URL || 'postgres:///triv_db' }
+// ));
+//
+// export default Bookshelf;
+
+const config = require('../config');
+
+const knex = require('knex')({client: 'pg', connection: config.databaseUrl });
+
+module.exports = require('bookshelf')(knex);
+
