9a038a4bfeb3cbaba5d63c951d7f9850c2fd0921
Scott Hill <scott289@gmail.com>
2016-11-18 11:40:02 -0800

    more es6

--- src/database/bookshelf-db.js
+++ src/database/bookshelf-db.js
@@ -1,19 +1,8 @@
-// import knex from 'knex';
-// import bookshelf from 'bookshelf';
-//
-// // Can't use config values here.
-// // import config  from '../config';
-//
-// const Bookshelf = bookshelf(knex(
-//   // {client: 'pg', connection: process.env.DATABASE_URL || 'postgres:///hills120' }
-//   {client: 'pg', connection: process.env.DATABASE_URL || 'postgres:///triv_db' }
-// ));
-//
-// export default Bookshelf;
+import config from '../config';
+import knex from 'knex';
+import bookshelf from 'bookshelf';
 
-const config = require('../config');
+const Bookshelf = bookshelf(
+  knex({client: 'pg', connection: config.databaseUrl }));
 
-const knex = require('knex')({client: 'pg', connection: config.databaseUrl });
-
-module.exports = require('bookshelf')(knex);
-
+export default Bookshelf;
--- src/server/index.js
+++ src/server/index.js
@@ -7,13 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(morgan("dev"));
 
-// If this is prod, serve the client build dir and set port
-if (process.env.NODE_ENV === "production") {
-  app.set('port', (process.env.PORT || 3001));
-  app.use(express.static(path.join(__dirname, '../client/build')));
-} else {
-  app.set('port', (process.env.SERVER_PORT || 3001));
-}
+app.set('port', (process.env.PORT || 3001));
 
 // Enable routes with /api prefix
 const api_routes = require('./api_routes');
