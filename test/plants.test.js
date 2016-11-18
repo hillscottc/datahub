801265d656d59b0066f0f7176b05c516bd15eab2
Scott Hill <scott289@gmail.com>
2016-11-18 14:43:47 -0800

    tests done

--- test/plants.test.js
+++ test/plants.test.js
@@ -1,15 +1,17 @@
 import should from 'should';
 import supertest from 'supertest';
-
-
+import querystring from 'querystring';
 import '../src/server/index.js';
+
+const server = supertest.agent("http://localhost:3001");
 
 
 describe('Plants API', function() {
-  var url = 'http://localhost:3001';
+
   describe('plants', function() {
-    it('basic get', function(done) {
-      supertest(url)
+
+    it('GET, no args', function(done) {
+      server
         .get('/api/plants')
         .end(function(err, res) {
           if (err) { throw err }
@@ -19,108 +21,71 @@ describe('Plants API', function() {
           done();
         });
     });
+
+
+    it("GET query", function (done) {
+      const payload = {family:'Malva', common: 'musk'};
+      server
+          .get("/api/plants/?" + querystring.stringify(payload))
+          .expect("Content-type", /json/)
+          .expect(200)
+          .end(function (err, res) {
+            res.body.data.length.should.equal(2);
+            done();
+          });
+    });
+
+
+    it("POST no args", function (done) {
+      const payload = {};
+      server
+          .post("/api/plants/")
+          .send(payload)
+          .expect("Content-type",/json/)
+          .expect(200)
+          .end(function(err, res){
+            const {data, pagination} = res.body;
+            data.length.should.equal(10);
+            pagination.rowCount.should.equal(90986);
+            done();
+          });
+    });
+
+
+    it("POST search symbol", function (done) {
+
+      const payload = {symbol:'ABELM'};
+
+      server
+          .post("/api/plants/")
+          .send(payload)
+          .expect("Content-type",/json/)
+          .expect(200)
+          .end(function(err, res){
+            const {data, pagination} = res.body;
+            // console.log({length: data.length, pagination});
+            const firstPlant = data[0];
+            firstPlant.symbol.should.equal("ABELM");
+            done();
+          });
+    });
+
+
+    it("POST musk okra query ",function(done) {
+
+      const payload = {family:'Malva', common: 'musk'};
+
+      server
+          .post("/api/plants/")
+          .send(payload)
+          .expect("Content-type",/json/)
+          .expect(200)
+          .end(function(err, res){
+            res.body.data.length.should.equal(2);
+            done();
+          });
+    });
+
   });
+
 });
-
-
-// const supertest = require("supertest");
-// const should = require("should");
-// const querystring = require('querystring');
-//
-// const server = supertest.agent("http://localhost:3001");
-//
-//
-// describe("BOOKSHELF GET TEST",function() {
-//
-//   it("plants all", function (done) {
-//     server
-//         .get("/api/plants/")
-//         .expect("Content-type", /json/)
-//         .expect(200)
-//         .end(function (err, res) {
-//           const {data, pagination} = res.body;
-//           data.length.should.equal(10);
-//           pagination.rowCount.should.equal(90986);
-//           done();
-//         });
-//   });
-//
-//
-//   it("plants query", function (done) {
-//
-//     const payload = {family:'Malva', common: 'musk'};
-//
-//     server
-//         .get("/api/plants/?" + querystring.stringify(payload))
-//         .expect("Content-type", /json/)
-//         .expect(200)
-//         .end(function (err, res) {
-//           res.body.data.length.should.equal(2);
-//           done();
-//         });
-//   });
-//
-//
-// });
-//
-//
-// describe("BOOKSHELF POST TEST",function() {
-//
-//
-//   it("plants search all", function (done) {
-//     const payload = {};
-//     server
-//         .post("/api/plants/")
-//         .send(payload)
-//         .expect("Content-type",/json/)
-//         .expect(200)
-//         .end(function(err, res){
-//           const {data, pagination} = res.body;
-//           data.length.should.equal(10);
-//           pagination.rowCount.should.equal(90986);
-//           done();
-//         });
-//   });
-//
-//
-//   it("plants search symbol", function (done) {
-//
-//     const payload = {symbol:'ABELM'};
-//
-//     server
-//         .post("/api/plants/")
-//         .send(payload)
-//         .expect("Content-type",/json/)
-//         .expect(200)
-//         .end(function(err, res){
-//           const {data, pagination} = res.body;
-//           console.log({length: data.length, pagination});
-//           const firstPlant = data[0];
-//           firstPlant.symbol.should.equal("ABELM");
-//           done();
-//         });
-//   });
-//
-//
-//   it("plants musk okra query ",function(done) {
-//
-//     const payload = {family:'Malva', common: 'musk'};
-//
-//     server
-//         .post("/api/plants/")
-//         .send(payload)
-//         .expect("Content-type",/json/)
-//         .expect(200)
-//         .end(function(err, res){
-//           res.body.data.length.should.equal(2);
-//           done();
-//         });
-//   });
-//
-// });
-//
-//
-//
-//
-//
-//
