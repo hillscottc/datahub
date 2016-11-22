d713c2786ee53e8a7a8170b7df5ae739b5f821e2
Scott Hill <scott289@gmail.com>
2016-11-22 11:22:42 -0800

    trying to set schema

--- src/database/bookshelf-db.js
+++ src/database/bookshelf-db.js
@@ -1,8 +1,28 @@
 import config from '../config';
 import knex from 'knex';
 import bookshelf from 'bookshelf';
+import Promise from 'bluebird';
 
 const Bookshelf = bookshelf(
-  knex({client: 'pg', connection: config.databaseUrl }));
+  knex({
+    client: 'pg',
+    debug: 'true',
+    connection: config.databaseUrl,
+    // connection: {
+    //   host: 'localhost',
+    //   user: 'hills120',
+    //   // password: settings.postgres.password,
+    //   // database: settings.postgres.database,
+    //   // port: settings.postgres.port
+    // },
+    // pool: {
+    //   afterCreate: function (connection, callback) {
+    //     Promise.promisify(connection.query,
+    //       connection)("SET SESSION SCHEMA 'trivia';", []).then(function () {
+    //       callback(null, connection);
+    //     });
+    //   }
+    // }
+  }));
 
 export default Bookshelf;
---
+++ src/server/temp.js
@@ -0,0 +1,25 @@
+import {Clue, Category} from '../database/trivia-schema';
+
+
+const limit = 3;
+const offset = 0;
+const cat_id = 307;
+const clue_id = 1633;
+
+
+Category.forge()
+  .query((qb) => {
+    qb.limit(2);
+    // qb.join('trivia.category', 'clue.category_id', 'category.id');
+  })
+  // .fetchPage({limit, offset})
+  .fetchAll()
+  // .fetch({withRelated: ['clues'], withSchema: 'trivia'})
+  .then((items) => {
+
+    console.log(items.toJSON());
+
+  })
+  .catch((err) => {
+    console.error(err)
+  });
