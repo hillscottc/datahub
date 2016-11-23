36245097789ec938663b658c01340ab69b97cf39
Scott Hill <scott289@gmail.com>
2016-11-22 21:49:14 -0800

    works

--- src/server/api_trivia.js
+++ src/server/api_trivia.js
@@ -3,90 +3,89 @@
  */
 import {Clue, Category} from '../database/trivia/schema';
 import express from 'express';
-import pg from 'pg';
-import config from '../config';
 
 const router = express.Router();
 
 
 // GET /api/clues -- RANDOM clues with optional limit (10)
 router.get('/clues/:limit?', (req, res) => {
-  const {limit=10} = req.params;
-  const results = [];
-  pg.connect(config.databaseUrl, (err, client, done) => {
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({ success: false, data: err});
-    }
+  const limit = +req.params.limit || 10;
 
-    // join with category name
-    const query = client.query(
-      "SELECT clue.id AS clue_id, category.category_name AS category, clue.question, clue.answer FROM trivia.clue " +
-      "JOIN trivia.category ON category.id = clue.category_id " +
-      "ORDER BY random() LIMIT " + limit + ";");
-
-    query.on('row', (row) => {
-      results.push(row);
+  Clue.forge()
+    .query((qb) => {
+      qb.limit(limit);
+      qb.orderByRaw('random()');
+    })
+    .fetchAll()
+    .then((clues) => {
+      // fix it ... id -> clue_id
+      return clues.models.map(clue => {
+        return Clue.fixId(clue.attributes)
+      });
+    })
+    .then((clues) => {
+      clues.length.should.equal(10);
+      return res.json(clues);
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.json({});
     });
-    query.on('end', () => {
-      done();
-      return res.json(results);
-    });
-  });
-
-
 
 });
 
 
 // GET /api/clues/cat/{id} -- clues by cat id
 router.get('/clues/cat/:id', (req, res) => {
-  const id = +req.params.id;
-  const results = [];
-  pg.connect(config.databaseUrl, (err, client, done) => {
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({ success: false, data: err});
-    }
-    const query = client.query(
-      "SELECT clue.id AS clue_id, category.category_name AS category, clue.question, clue.answer FROM trivia.clue " +
-      "JOIN trivia.category ON category.id = clue.category_id " +
-      "WHERE clue.category_id = $1;", [id]);
-    query.on('row', (row) => {
-      results.push(row);
+  const id = req.params.id;
+
+  Category.forge({id})
+    .fetch({withRelated: ['clues']})
+    .then((cat) => {
+      return cat.related('clues');
+    })
+    .then((clues) => {
+      // fix it ... id -> clue_id
+      return clues.models.map(clue => {
+        return Clue.fixId(clue.attributes)
+      });
+    })
+    .then((clues) => {
+      return res.json(clues);
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.json({});
     });
-    query.on('end', () => {
-      done();
-      return res.json(results);
-    });
-  });
+
 });
 
 
 // GET /api/cats -- RANDOM categories
 router.get('/cats/:limit?', (req, res) => {
   const {limit=10} = req.params;
-  const results = [];
-  pg.connect(config.databaseUrl, (err, client, done) => {
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({ success: false, data: err});
-    }
-    // const query = client.query("SELECT * FROM trivia.category ORDER BY random() LIMIT " + limit + ";");
-    const query = client.query(
-      "SELECT id as category_id, category_name FROM trivia.category " +
-      "ORDER BY random() LIMIT " + limit + ";");
-    query.on('row', (row)  =>{
-      results.push(row);
+
+  Category.forge()
+    .query((qb) => {
+      qb.limit(limit);
+      qb.orderByRaw('random()');
+    })
+    .fetchAll()
+    .then((cats) => {
+      // fix it ... id -> clue_id
+      return cats.models.map(cat => {
+        return Category.fixId(cat.attributes)
+      });
+    })
+    .then((cats) => {
+      // clues.length.should.equal(10);
+      return res.json(cats);
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.json({});
     });
-    query.on('end', ()  =>{
-      done();
-      return res.json(results);
-    });
-  });
+
 });
 
 
